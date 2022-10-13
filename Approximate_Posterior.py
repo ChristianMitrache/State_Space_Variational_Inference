Organized Testcase files, refactored some block matrix functions, fixed documentation, got rid of some redundant class variables, reparameterized variance model to inverse variance model

Approximate_Posterior.py
@@ -1,7 +1,7 @@
 # Abstract Class format taken directly from:  https://github.com/earcher/vilds/blob/master/code/RecognitionModel.py
 # with significant changes made.
 import math
-
+from Block_Functions import Block_Mat_Vec
 import torch
 from torch import nn
 
@@ -39,38 +39,27 @@ class Time_Series_Approx_Gaussian_First_Param(Recognition_Model):
     def __init__(self, time_steps,mean_model,cov_model):
         self.mean_model = mean_model
         self.cov_model = cov_model
-        self.time_steps = time_steps
         super().__init__()
-
-    def eval_entropy(self,x):
-        '''
-        Evaluating Gaussian entropy in equation (9) in paper and returning it. Given diagonal blocks of cholesky
-        decomposition for the variance.
-        '''
-        # getting model outputs
-        # getting cholesky factors for mean_cov_model
-        D,B = self.cov_model(x)
-        return (self.x_dim*self.time_steps/2)*(1 + torch.log(2*torch.tensor(math.pi))) +\
-               torch.sum(torch.log(torch.diagonal(D,dim1 = 1,dim2=2)))
 
     def get_Sample(self,n,x):
         """
-        Implementing function that samples from approximate posterior.
-        :return: Returns tensor of samples of size n x T x dim_z
+        Returns batched samples from time series model given observational vector x.
+        Note: Autograd does not record
+        :param n: number of batches.
+        :param x: observational time series x
+        :return: Returns batch x T x x_dim tensor of normal samples
         """
-        with torch.no_grad:
-            mean = self.mean_model(x)
-            epsilon = torch.normal(size=(n,self.time_steps,self.cov_model.xt_dim))
-            noise_cov_term=  self.cov_model
-            #noise_cov_term = self.cov_model.block_inverse_solve(self.D, self.B, epsilon)
-            return torch.unsqueeze(mean,dim =0) + noise_cov_term
+        epsilon = torch.normal(size=(n, x.shape[0], x.shape[1]),requires_grad= False)
+        noise_cov_term = Block_Mat_Vec(self.D, self.B, epsilon)
+        mean = self.mean_model(x)
+        return torch.unsqueeze(mean,dim = 0) + noise_cov_term
 
     def forward(self,x):
         """
-        Implementing forward pass required by nn module. This gives the.
-        :param x:
-        :return: the entropy of the
+        Implementing forward pass required by nn module.
+        :param x: observations from time series (Should be a T x dim_x vector)
+        :return:  entropy value of the normal distributions centered at each data-point in time series.
         """
-        self.D,self.B = self.cov_model(x)
-        return (self.x_dim*self.time_steps/2)*(1 + torch.log(2*torch.tensor(math.pi))) +\
-               torch.sum(torch.log(torch.diagonal(self.D,dim1 = 1,dim2=2)))
+        self.D= self.cov_model(x)[0]
+        return (x.shape[0]*x.shape[1]/2)*(1 + torch.log(2*torch.tensor(math.pi))) - \
+               torch.sum(torch.log(torch.diagonal(self.D,dim1 = 1,dim2=2)),dim=1)

Mean_Cov_Models.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-from Block_Functions import compute_block_cholesky
+from Block_Functions import Compute_Block_Cholesky
 
 class Fully_Connected_Model(nn.Module):
     '''
@@ -52,7 +52,7 @@ class Fully_Connected_Mean_Model(Fully_Connected_Model):
     def __init__(self,xt_dim,linear_layer_dims,non_lin_module):
         super().__init__(xt_dim,linear_layer_dims,non_lin_module)
 
-class Variance_Model(nn.Module):
+class Inverse_Variance_Model(nn.Module):
     '''
     Variance model for parameterization 1 of the variational inference for state space models paper.
     models the block diagonal and off-block diagonal matrices for the lower-triangle of the cholesky decomposition
@@ -74,7 +74,7 @@ class Variance_Model(nn.Module):
         Computes forward pass and returns diagonal blocks of block cholesky decomp.
         x should be a Txn tensor.
         returns a tuple of ((Tx n x n), (T-1 x n x n)) matrices representing the block matrices for the lower diagonal
-        matrix.
+        matrix of the inverse covariance.
         """
 
         x = torch.unsqueeze(x, dim=2)
@@ -83,4 +83,4 @@ class Variance_Model(nn.Module):
         # Not sure if it's faster do the unsqueezing below or to just deal with this using masking - will have to test this
         L = torch.tril(L,diagonal=-1) + (torch.unsqueeze(torch.exp(torch.diagonal(L,dim1 = 1,dim2=2)),dim = 2)*torch.eye(L.shape[1]))
         D = L@torch.transpose(L,dim0=1,dim1=2)
-        return compute_block_cholesky(D,B)
+        return Compute_Block_Cholesky(D, B)

Tests/Block_Functions_Tests.py
@@ -1,21 +1,21 @@
 import unittest
 
-from Mean_Cov_Models import Variance_Model
+from Mean_Cov_Models import Inverse_Variance_Model
 import torch
-from Block_Functions import compute_block_cholesky
+from Block_Functions import Compute_Block_Cholesky
 
 class Test_Block_Cholesky(unittest.TestCase):
     def test_block_cholesky_diagonal(self):
         B = torch.zeros((9,1,1))
         D = torch.tensor([[[1.]], [[2.]], [[3.]], [[4.]], [[5.]], [[6.]], [[7.]], [[8.]], [[9.]], [[10.]]])
-        chol_tuple = compute_block_cholesky(D, B)
+        chol_tuple = Compute_Block_Cholesky(D, B)
         self.assertTrue(torch.allclose(chol_tuple[0]**2, D) and torch.allclose(chol_tuple[1],B)) # add assertion here
 
     def test_block_cholesky_non_diagonal(self):
         D = torch.exp(torch.rand((10,1,1)))
         B = torch.rand((9,1,1))
         A = torch.diag(torch.squeeze(D)) + torch.diag_embed(torch.squeeze(B),offset  =-1)
-        chol_tuple = compute_block_cholesky(D, B)
+        chol_tuple = Compute_Block_Cholesky(D, B)
         L_block_chol = torch.diag(torch.squeeze(chol_tuple[0])) + torch.diag_embed(torch.squeeze(chol_tuple[1]),offset  =-1)
         L_torch_chol = torch.linalg.cholesky_ex(A)[0]
         self.assertTrue(torch.allclose(L_torch_chol,L_block_chol)) # add assertion here
@@ -29,7 +29,7 @@ class Test_Block_Cholesky(unittest.TestCase):
         # Constructing matrix to feed into torch from this:
         #cat_tensor = torch.cat((D[:-1, :, :], B), dim=1)
         # rearranging cat_tensor to represent it as 2-d Array
-        chol_tuple = compute_block_cholesky(D, B)
+        chol_tuple = Compute_Block_Cholesky(D, B)
         A = reshape_helper_D(D, B,lower = False)
         L_block_chol = reshape_helper_D(chol_tuple[0],chol_tuple[1],lower = True)
         L_torch_chol = torch.linalg.cholesky_ex(A)[0]
@@ -44,7 +44,7 @@ class Test_Block_Cholesky(unittest.TestCase):
         # Constructing matrix to feed into torch from this:
         #cat_tensor = torch.cat((D[:-1, :, :], B), dim=1)
         # rearranging cat_tensor to represent it as 2-d Array
-        chol_tuple = compute_block_cholesky(D, B)
+        chol_tuple = Compute_Block_Cholesky(D, B)
         A = reshape_helper_D(D, B,lower = False)
         L_block_chol = reshape_helper_D(chol_tuple[0],chol_tuple[1],lower = True)
         L_torch_chol = torch.linalg.cholesky_ex(A)[0]
@@ -59,7 +59,7 @@ class Test_Block_Cholesky(unittest.TestCase):
         # Constructing matrix to feed into torch from this:
         #cat_tensor = torch.cat((D[:-1, :, :], B), dim=1)
         # rearranging cat_tensor to represent it as 2-d Array
-        chol_tuple = compute_block_cholesky(D, B)
+        chol_tuple = Compute_Block_Cholesky(D, B)
         A = reshape_helper_D(D, B,lower = False)
         L_block_chol = reshape_helper_D(chol_tuple[0],chol_tuple[1],lower = True)
         L_torch_chol = torch.linalg.cholesky_ex(A)[0]
@@ -86,20 +86,3 @@ def reshape_helper_D(D,B,lower = False):
         return D_block_diag + B_block_full
     else:
         return D_block_diag + B_block_full + B_block_full.T
-
-########################################################################################################
-
-class Test_Variance_Model(unittest.TestCase):
-    def test_variance_model_shapes(self):
-        time = 100
-        xt_dim = 10
-        x = torch.rand((100,10))
-        linear_layers = (7,2,7)
-        non_linearity = torch.nn.ReLU()
-        variance_model = Variance_Model(xt_dim,linear_layers,non_linearity)
-        L_D, L_B = variance_model(x)
-        self.assertTrue(L_B.shape ==(99,10,10) and L_D.shape == (100,10,10))
-
-
-if __name__ == '__main__':
-    unittest.main()