06fe2d132bcf945b515eb5bc4cb3e17b7dd84015
Christian Mitrache <christianstatsmitrache@gmail.com>
2022-10-19 10:41:41 -0400

    One Way of implementing Poisson_LDS

--- Poisson_LDS.py
+++ Poisson_LDS.py
@@ -1,10 +1,35 @@
-from User_Model import Expected_Joint_Likelihood
+import torch
+from torch import nn
+from torch.distributions import Poisson, MultivariateNormal
 
-class Poisson_LDS(Expected_Joint_Likelihood):
+class Poisson_LDS_Expected_Likelihood(nn.Module):
     """
+    Class that initializes Poisson LDS likelihood
+    """
+    def __init__(self, epsilon_precision: torch.Tensor,LDS_matrix: torch.Tensor,
+                 loading_matrix: torch.Tensor,loading_bias: torch.Tensor):
+        """
+        Specifying class that takes in model parameters and feeds the corresponding dictionary to Super-Class
+        In the notation of https://arxiv.org/pdf/1511.07367.pdf
+        :param epsilon_precision: Q^-1
+        :param LDS_matrix: A
+        :param loading_matrix: C NOTE: C must be z_t x x_t matrix
+        :param loading_bias: d
+        """
+        super().__init__()
+        self.params = nn.ParameterDict({'epsilon_precision': epsilon_precision,
+                                        'LDS_mat': LDS_matrix,
+                                        'loading_mat':loading_matrix,
+                                        'loading_bias':loading_bias})
 
-    """
-    def __init__(self):
+    def forward(self,x:torch.Tensor,z:torch.Tensor):
         """
-        Constructor for model
+        This method must be
+        :param x: a batch x Tx dim_x vector corresponding to the observations of the time series
+        :param z: a single sample of size T x dim_x corresponding to a sample drawn from the approximate posterior.
+        :return: Expected Log Joint Likelihood: P_{\theta}(x,z)
         """
+        MultivariateNormal(loc = 0,precision_matrix=self.params['epsilon_precision']).log_prob(z[0,:,:]) + \
+        torch.sum(MultivariateNormal(loc = z@self.params['LDS_mat'].T,
+                                     precision_matrix=self.params['epsilon_precision']).log_prob(z[1:,:,:])) + \
+        torch.sum(Poisson(rate = (z@self.params['loading_matrix'] + self.params['loading_bias'])).log_prob(x))
--- User_Model.py
+++ User_Model.py
@@ -1,54 +1,30 @@
+import torch
 from torch import nn
+from typing import Dict
 
-class Joint_Likelihood(nn.Module):
+# NOT SURE IF I AM GONNA KEEP THIS CLASS - KINDA SEEMS LIKE A WASTE OF TIME.
+
+@torch.jit.script
+class Expected_Joint_Likelihood(nn.Module):
     """
     Astract class that must be implemented by the user when specifying a model for the data.
-    This class allows the user to specify the joint likelihood for a single sample z (for convenience)
+    This classes only function is to remind the user what needs to be implemented in the forward pass and
+    to force the user to register the model parameters.
     """
-    def __init__(self, **named_parameters):
+    def __init__(self, named_model_parameters: Dict[str,torch.Tensor(float)]):
         """
         This method takes in a dictionary of model parameters
         :param named_model_parameters: keyword arguments
         """
         # registering the keyword arguments specified as parameters of the model
         super().__init__()
-        for parameter_name in named_parameters:
-            print(parameter_name)
-            #print(parameter_name)
-            #print(named_model_parameters[parameter_name])
-            #self.vars()[parameter_name] = nn.Parameter(named_model_parameters[parameter_name],requires_grad= True)
+        self.params = nn.ParameterDict(named_model_parameters)
 
-    def forward(self,x,z):
+    def forward(self,x,z) -> torch.Tensor:
         """
         This method must be
         :param x: a Tx dim_x vector corresponding to the observations of the time series
         :param z: a single sample of size T x dim_x corresponding to a sample drawn from the approximate posterior.
         :return: Joint-likelihood: P_{\theta}(x,z)
-        """
-        raise Exception("Implement me. This is an abstract method.")
-
-
-class Expected_Joint_Likelihood(nn.Module):
-    """
-    Astract class that must be implemented by the user when specifying a model for the data.
-    This class allows the user to directly implement
-    """
-
-    def __init__(self):
-        """
-        This method takes in a dictionary of model parameters
-        :param model_parameters: varying length sequence of model parameters
-        """
-        # registering the keyword arguments specified as parameters of the model
-        super().__init__()
-        for parameter_name, in named_model_parameters:
-            self.vars()[parameter_name] = nn.Parameter(named_model_parameters[parameter_name],requires_grad= True)
-
-    def forward(self,x,z):
-        """
-        This method must be
-        :param x: a Tx dim_x vector corresponding to the observations of the time series
-        :param z: batch of samples of size batch x T x dim_x corresponding to a sample drawn from the approximate posterior.
-        :return: Estimate of expected Joint-likelihood: E_{z~q}(P_{\theta}(x,z))
         """
         raise Exception("Implement me. This is an abstract method.")
