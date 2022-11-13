- Debugging Trainer and Training Loop - Attempting to make Inverse Variance Model more numerically stable - Started Work on Fixing batched-Entropy for Approximate Posterior.

Mean_Cov_Models.py
@@ -64,6 +64,7 @@ class Inverse_Variance_Model(nn.Module):
         self.model_D = Fully_Connected_Model(xt_dim,zt_dim**2, linear_layer_dims_D, non_lin_module)
         self.final_weights_D_z = nn.Parameter(torch.rand(size=(zt_dim, zt_dim), requires_grad=True))
         self.non_linearity = non_lin_module
+        self.batch_norm = torch.nn.BatchNorm1d(zt_dim,track_running_stats= False)
 
     def forward(self, x):
         """
@@ -78,13 +79,17 @@ class Inverse_Variance_Model(nn.Module):
 
         # reshaping before passing into linear models (not sure if I should do the reshaping after - maybe incorporate
         # some 2d convolutions? -consider convolutional network with large convolutional range?)
-        B = self.non_linearity(self.final_weights_B_z * torch.reshape(self.model_B(x[:-1,:]),(x.shape[0]-1,self.zt_dim,self.zt_dim)))
-        D  = self.non_linearity(self.final_weights_D_z * torch.reshape(self.model_D(x), (x.shape[0],self.zt_dim, self.zt_dim)))
+
+        # Designing architecture to produce numerically stable matrices for cholesky decomposition. (by applying a batch normalization after)
+        B = self.batch_norm(self.non_linearity(self.final_weights_B_z * torch.reshape(self.model_B(x[:-1,:]),(x.shape[0]-1,self.zt_dim,self.zt_dim))))*10
+        D  = self.batch_norm(self.non_linearity(self.final_weights_D_z * torch.reshape(self.model_D(x), (x.shape[0],self.zt_dim, self.zt_dim))))*10
 
         # Making sure D is strictly positive definite,symmetric
         # Not sure if it's faster do the unsqueezing below or to just deal with this using masking - will have to test this
+        D = (10/D.shape[1])*(D @ torch.transpose(D, dim0=1, dim1=2)) # Making Matrix symmetric and normalizing values closer to N(1,1)
 
-        D = torch.tril(D,diagonal=-1) + (torch.unsqueeze(torch.exp(torch.diagonal(D,dim1 = 1,dim2=2)),dim = 2)*torch.eye(D.shape[1])) + \
-            torch.transpose(torch.tril(D,diagonal=-1),dim0=1,dim1=2)
-        D = D@torch.transpose(D,dim0=1,dim1=2)
+        #D = torch.tril(D,diagonal=-1) + (torch.unsqueeze(torch.exp(torch.diagonal(D,dim1 = 1,dim2=2)),dim = 2)*torch.eye(D.shape[1])) + \
+        #    torch.transpose(torch.tril(D,diagonal=-1),dim0=1,dim1=2)
+
+        D = torch.exp(D)
         return Compute_Block_Cholesky(D, B)

Poisson_LDS.py
@@ -4,6 +4,7 @@ import torch
 from torch import nn
 from torch.distributions import Poisson, MultivariateNormal
 
+
 def sample_from_Poisson_LDS(epsilon_precision: torch.Tensor, LDS_matrix: torch.Tensor,
                             loading_matrix: torch.Tensor, loading_bias: torch.Tensor, time_steps: int):
     """
@@ -19,18 +20,19 @@ def sample_from_Poisson_LDS(epsilon_precision: torch.Tensor, LDS_matrix: torch.T
     Note: Batching for this function is not implemented.
     """
     # sampling from epsilon process.
-    sampled_epsilon = MultivariateNormal(loc=torch.zeros(time_steps,epsilon_precision.shape[0]),
-                                         precision_matrix=epsilon_precision).sample((time_steps,epsilon_precision.shape[0]))
+    sampled_epsilon = MultivariateNormal(loc=torch.zeros(epsilon_precision.shape[0]),
+                                         precision_matrix=epsilon_precision).sample(
+        (time_steps,))
 
-    #initial sample assuming that z_0 = epsilon_0
-    sample_tensor = torch.zeros((time_steps,loading_matrix.shape[0]))
-    z_curr = sampled_epsilon[0,:]
-    sample_tensor[0,:] = Poisson(rate = torch.exp(loading_matrix@z_curr+loading_bias))
+    # initial sample assuming that z_0 = epsilon_0
+    sample_tensor = torch.zeros((time_steps, loading_matrix.shape[0]))
+    z_curr = sampled_epsilon[0, :]
+    sample_tensor[0, :] = torch.poisson(torch.exp(z_curr @ loading_matrix.T + loading_bias))
 
     # Iterative sampling assuming z_t = Az_{t-1} + epsilon_t
-    for i in range(1,time_steps):
-        z_curr =  LDS_matrix@z_curr + sampled_epsilon[i,:]
-        sample_tensor[i,:] = Poisson(rate = torch.exp(loading_matrix@z_curr+loading_bias))
+    for i in range(1, time_steps):
+        z_curr = LDS_matrix @ z_curr + sampled_epsilon[i, :]
+        sample_tensor[i, :] = torch.poisson(torch.exp(z_curr @ loading_matrix.T + loading_bias))
     return sample_tensor
 
 
@@ -38,7 +40,8 @@ class Poisson_LDS_Expected_Likelihood(nn.Module):
     """
     Class that initializes Poisson LDS likelihood
     """
-    def __init__(self, xt_dim,zt_dim, epsilon_precision = None, LDS_matrix= None, loading_matrix= None, loading_bias = None):
+
+    def __init__(self, xt_dim, zt_dim, epsilon_precision=None, LDS_matrix=None, loading_matrix=None, loading_bias=None):
         """
         Specifying class that takes in model parameters and feeds the corresponding dictionary to Super-Class
         In the notation of https://arxiv.org/pdf/1511.07367.pdf
@@ -57,26 +60,27 @@ class Poisson_LDS_Expected_Likelihood(nn.Module):
         # epsilon covariance:
         if epsilon_precision is None:
             # Making this positive semi-definite
-            epsilon_precision = torch.rand((zt_dim,zt_dim))
+            epsilon_precision = torch.rand((zt_dim, zt_dim))
             epsilon_precision = epsilon_precision @ epsilon_precision.T
             # Exponentiating diagonal entries to make it positive definite
             diag_mask = torch.diag(torch.ones(epsilon_precision.shape[0]))
-            epsilon_precision = diag_mask * torch.exp(torch.diag(epsilon_precision)) + (1. - diag_mask) * epsilon_precision
-        self.epsilon_precision = nn.Parameter(epsilon_precision,requires_grad= True)
+            epsilon_precision = diag_mask * torch.exp(torch.diag(epsilon_precision)) + (
+                        1. - diag_mask) * epsilon_precision
+        self.epsilon_precision = nn.Parameter(epsilon_precision, requires_grad=True)
 
         # Linear Dynamic system of z_t = Az_{t-1} + epsilon_t
-        self.LDS_transformation = nn.Linear(zt_dim,zt_dim,bias=False)
+        self.LDS_transformation = nn.Linear(zt_dim, zt_dim, bias=False)
         if LDS_matrix is None:
-            self.LDS_transformation.weight = nn.Parameter(torch.rand((zt_dim,zt_dim)),requires_grad=True)
+            self.LDS_transformation.weight = nn.Parameter(torch.rand((zt_dim, zt_dim)), requires_grad=True)
 
         # Loading Transformation for poisson rate:
-        self.loading_transformation  = nn.Linear(zt_dim,xt_dim)
+        self.loading_transformation = nn.Linear(zt_dim, xt_dim)
         if loading_matrix is None:
-            self.loading_transformation.weight = nn.Parameter(torch.rand(xt_dim,zt_dim),requires_grad=True)
+            self.loading_transformation.weight = nn.Parameter(torch.rand(xt_dim, zt_dim), requires_grad=True)
         if loading_bias is None:
             self.loading_transformation.bias = nn.Parameter(torch.rand(xt_dim))
 
-    def forward(self,x:torch.Tensor,z:torch.Tensor,batched =True):
+    def forward(self, x: torch.Tensor, z: torch.Tensor, batched=True):
         """
         This method must be
         :param x: a tensor of size dim_x vector corresponding to the observations of the time series
@@ -87,9 +91,12 @@ class Poisson_LDS_Expected_Likelihood(nn.Module):
             return torch.mean(
                 torch.sum(MultivariateNormal(loc=self.LDS_transformation(z[:, :-1, :]),
                                              precision_matrix=self.epsilon_precision).log_prob(z[:, 1:, :]), dim=1) + \
-                torch.sum(Poisson(rate=torch.exp(self.loading_transformation(z[:,1:,:]))).log_prob(x[1:,:]), dim=(1, 2)))
+                torch.sum(Poisson(rate=torch.exp(self.loading_transformation(z[:, 1:, :]))).log_prob(x[1:, :]),
+                          dim=(1, 2)))
         else:
-            return torch.mean(MultivariateNormal(loc = torch.zeros(z.shape[2]),precision_matrix=self.epsilon_precision).log_prob(z[:,0,:]) + \
-            torch.sum(MultivariateNormal(loc = self.LDS_transformation(z[:,:-1,:]),
-                                         precision_matrix= self.epsilon_precision).log_prob(z[:,1:,:]),dim = 1) + \
-            torch.sum(Poisson(rate = torch.exp(self.loading_transformation(z))).log_prob(x),dim=(1,2)))
+            return torch.mean(
+                MultivariateNormal(loc=torch.zeros(z.shape[2]), precision_matrix=self.epsilon_precision).log_prob(
+                    z[:, 0, :]) + \
+                torch.sum(MultivariateNormal(loc=self.LDS_transformation(z[:, :-1, :]),
+                                             precision_matrix=self.epsilon_precision).log_prob(z[:, 1:, :]), dim=1) + \
+                torch.sum(Poisson(rate=torch.exp(self.loading_transformation(z))).log_prob(x), dim=(1, 2)))

Trainer.py
@@ -21,11 +21,11 @@ class Approximate_Inference_Loss(Module):
         Makes calls to approximate model class and estimates the KL divergence between the user model and
         and the approximate model.
         :param n: number of samples to take when estimating expectation
-        :param x: observations of the time series (a tensor of shape batch_time x x_dim )
+        :param x: observations of the time series (a tensor of shape batch_time x x_dim)
         :return: returns the KL divergence between approximate and user model.
         """
-        samples = self.Approx_Post.Sample(n, x)  # Batch x time x x_dim
-        return self.Approx_Post(x) + self.User_model(x,samples)
+        # Note: Sampling needs to be called after forward pass of approximate posterior!
+        return self.Approx_Post(x) + self.User_model(x,self.Approx_Post.Sample(n, x))
 
 
 # Dataloader for getting subsets of original time series
@@ -49,16 +49,16 @@ def Create_Time_Series_Data_Loader(X:torch.Tensor,batch_size:int,num_workers = 0
 
 
 def train_model_epoch(loader,loss_fn,optimizer,scheduler,
-                      device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')):
+                      num_samples = 100,device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')):
   """Train model for one epoch and return loss"""
 
-  for batch_index, (data) in enumerate(loader):
+  for batch_index, data in enumerate(loader):
       # Moving Data to GPU (and converting ints to floating points for training)
-      data = data.float().to(device = device)
+      data = data[0].float().to(device = device)
 
       # Forward pass:
       optimizer.zero_grad()
-      loss = loss_fn(data)
+      loss = loss_fn(data, num_samples)
 
       # zero out gradients and perform backward pass.
       loss.backward()
@@ -69,5 +69,6 @@ def train_model_epoch(loader,loss_fn,optimizer,scheduler,
        # print(batch_index+1)
        # print("model_loss for batch")
        # print(loss.item())
-  scheduler.step()
+
+  scheduler.step() # change learning rate after epoch with scheduler
   return loss

main.py
@@ -21,7 +21,6 @@ if __name__ == "__main__":
     true_d = torch.rand(xt_dim)
     # sampling from true Poisson LDS model.
     observed_time_series = Poisson_LDS.sample_from_Poisson_LDS(torch.inverse(true_Q),true_A,true_C,true_d,time)
-    print(observed_time_series.shape) # checking shape
 
     # instantiating random initialization of a poisson LDS model to be trained
     PLDS_model = Poisson_LDS_Expected_Likelihood(xt_dim, zt_dim)
@@ -39,6 +38,7 @@ if __name__ == "__main__":
     # hyper-parameters for training:
     batch_size = 10
     num_epochs = 100
+    num_samples = 100
     lr = 1e-4
 
     # Setting Device (GPU/CPU)
@@ -62,7 +62,8 @@ if __name__ == "__main__":
         print("Epoch:")
         print(i)
         print("batch loss at end of epoch:")
-        last_batch_loss = Trainer.train_model_epoch(time_series_loader, KL_Divergence, optimizer, scheduler).item()
+        last_batch_loss = Trainer.train_model_epoch(time_series_loader, KL_Divergence, optimizer, scheduler,
+                                                    num_samples=num_samples).item()
         print(last_batch_loss)
         training_losses.append(last_batch_loss)
 