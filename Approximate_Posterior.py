- Debugging Trainer and Training Loop - Attempting to make Inverse Variance Model more numerically stable - Started Work on Fixing batched-Entropy for Approximate Posterior.

Approximate_Posterior.py
@@ -76,7 +76,7 @@ class Time_Series_Approx_Gaussian_First_Param(Approximate_Model):
         """
         self.L_D,self.L_B = self.inv_cov_model(x)
         return (x.shape[0]*x.shape[1]/2)*(1 + torch.log(2*torch.tensor(math.pi))) - \
-               torch.sum(torch.log(torch.diagonal(self.L_D,dim1 = 1,dim2=2)),dim=(1))
+               0.5*torch.sum(torch.log(torch.diagonal(self.L_D,dim1 = 1,dim2=2)),dim=(0,1))
 
     def get_mean_parameters(self):
         """

Tests/Block_Functions_Tests.py
@@ -73,7 +73,7 @@ class Test_Block_Cholesky(unittest.TestCase):
         self.assertTrue(torch.allclose(L_torch_chol,L_block_chol)) # add assertion here
 
     def test_block_cholesky_non_diagonal_2_small_dim_unstable(self):
-        xt_dim = 3
+        xt_dim = 5
         time = 5
         L = torch.rand((time,xt_dim,xt_dim),dtype = torch.float64)
         D = torch.exp(L@ torch.transpose(L,dim0=1,dim1=2))

main.py
@@ -11,22 +11,26 @@ import numpy as np
 
 if __name__ == "__main__":
     # Constructing Random True PLDS:
-    xt_dim = 5
-    zt_dim = 2
+    xt_dim = 30
+    zt_dim = 10
     time = 200
     # instantiating some random true parameters:
     true_A = torch.rand((zt_dim,zt_dim))
     true_C = torch.rand((xt_dim,zt_dim))
-    true_Q = torch.tensor([[1,0.5],[0.5,1]])
+    true_Q = torch.rand((zt_dim,zt_dim))
+    true_Q = true_Q.T @ true_Q/true_Q.shape[0]
+    true_Q = torch.tril(true_Q,diagonal=-1) + \
+            torch.diag(torch.exp(torch.diag(true_Q))) + torch.tril(true_Q,diagonal=-1).T
+    print(true_Q.shape)
     true_d = torch.rand(xt_dim)
     # sampling from true Poisson LDS model.
-    observed_time_series = Poisson_LDS.sample_from_Poisson_LDS(torch.inverse(true_Q),true_A,true_C,true_d,time)
+    observed_time_series = Poisson_LDS.sample_from_Poisson_LDS(true_Q,true_A,true_C,true_d,time)
 
     # instantiating random initialization of a poisson LDS model to be trained
     PLDS_model = Poisson_LDS_Expected_Likelihood(xt_dim, zt_dim)
 
     # Constructing mean-covariance NNs
-    lin_layer_dims = (4,2,4)
+    lin_layer_dims = (40,50,60)
     non_lin_module = nn.ReLU()
     mean_model = Mean_Cov_Models.Fully_Connected_Mean_Model(xt_dim,zt_dim,lin_layer_dims,non_lin_module)
     inv_cov_model = Mean_Cov_Models.Inverse_Variance_Model(xt_dim,zt_dim,lin_layer_dims,lin_layer_dims,non_lin_module)
@@ -36,7 +40,7 @@ if __name__ == "__main__":
     KL_Divergence = Trainer.Approximate_Inference_Loss(Approx_model,PLDS_model)
 
     # hyper-parameters for training:
-    batch_size = 10
+    batch_size = 100
     num_epochs = 100
     num_samples = 100
     lr = 1e-4