Fixed NN architecture for covariance

Block_Functions.py
@@ -37,7 +37,7 @@ def Compute_Block_Cholesky(D: torch.Tensor, B: torch.Tensor) -> Tuple[torch.Tens
 def Block_Triangular_Solve(D:torch.Tensor, B:torch.Tensor, x:torch.Tensor) -> torch.Tensor:
     """
     Given cholesky diagonal blocks D, cholesky off-diagonal blocks B, solve for L^{-1}x where L is cholesky
-    decomposition lower triangle
+    lower triangle.
     :param D: cholesky diagonal blocks tensor with shape Txnxn
     :param B: cholesky off-diagonal blocks with shape (T-1)xnxn
     :param x: tensor with shape mxTxn where m is the batch dimension.
@@ -45,11 +45,11 @@ def Block_Triangular_Solve(D:torch.Tensor, B:torch.Tensor, x:torch.Tensor) -> to
     """
     return_vec = torch.zeros((x.shape[0],x.shape[1],x.shape[2]))
     # Initial solution (without block B_i)
-    solve_i = torch.linalg.solve_triangular(D[0,:,:],x[:,0,:].T,upper = False).T
+    solve_i = torch.linalg.solve_triangular(D[0,:,:], x[:,0,:].T,upper = False).T
     return_vec[:,0,:] = solve_i
     # Iterating through rest of blocks with B_i, D_i present in linear system
     for i in range(1,D.shape[0]):
-        solve_i = torch.linalg.solve_triangular(D[i,:,:],x[:,i,:].T- B[i-1,:,:]@ solve_i.T,upper = False).T
+        solve_i = torch.linalg.solve_triangular(D[i,:,:], x[:,i,:].T- B[i-1,:,:]@ solve_i.T,upper = False).T
         return_vec[:,i,:] = solve_i
     return return_vec
 

Mean_Cov_Models.py
@@ -60,7 +60,7 @@ class Inverse_Variance_Model(nn.Module):
         """
         super().__init__()
         self.zt_dim = zt_dim
-        self.model_B = Fully_Connected_Model(xt_dim,(zt_dim)**2,linear_layer_dims_B,non_lin_module)
+        self.model_B = Fully_Connected_Model(2*xt_dim,(zt_dim)**2,linear_layer_dims_B,non_lin_module)
         self.final_weights_B_z = nn.Parameter(torch.rand(size=(zt_dim, zt_dim), requires_grad=True))
         self.model_D = Fully_Connected_Model(xt_dim,zt_dim**2, linear_layer_dims_D, non_lin_module)
         self.final_weights_D_z = nn.Parameter(torch.rand(size=(zt_dim, zt_dim), requires_grad=True))
@@ -71,9 +71,7 @@ class Inverse_Variance_Model(nn.Module):
     def forward(self, x):
         """
         Computes forward pass and returns diagonal blocks of block cholesky decomp.
-        x should be a Txn tensor.
-
-        NOTE: THIS FUNCTION MIGHT NOT WORK FOR BATCHED TIME SERIES.
+        x should be a T x xt_dim tensor.
 
         returns a tuple of ((Tx n x n), (T-1 x n x n)) matrices representing the block matrices for the lower diagonal
         matrix of the inverse covariance.
@@ -81,16 +79,16 @@ class Inverse_Variance_Model(nn.Module):
 
         # reshaping before passing into linear models (not sure if I should do the reshaping after - maybe incorporate
         # some 2d convolutions? -consider convolutional network with large convolutional range?)
+        # Prepping inputs to model_B
 
         # Designing architecture to produce numerically stable matrices for cholesky decomposition. (by applying a batch normalization after)
-        B = self.final_weights_B_z * torch.reshape(self.model_B(x[:-1,:]),(x.shape[0]-1,self.zt_dim,self.zt_dim))
+        B = self.final_weights_B_z * torch.reshape(self.model_B(torch.cat((x[:-1,:],x[1:,:]),dim=1)),
+                                                   (x.shape[0]-1,self.zt_dim,self.zt_dim))
         D  = self.final_weights_D_z * torch.reshape(self.model_D(x), (x.shape[0],self.zt_dim, self.zt_dim))
 
-        # Making sure D is strictly positive definite,symmetric
-        # Not sure if it's faster do the unsqueezing below or to just deal with this using masking - will have to test this
-        D = (10/D.shape[1])*(D @ torch.transpose(D, dim0=1, dim1=2)) # Making Matrix symmetric and normalizing values closer to N(1,1)
-
-        D = torch.tril(D,diagonal=-1) + (torch.unsqueeze(torch.abs(torch.diagonal(D,dim1 = 1,dim2=2)),dim = 2)*torch.eye(D.shape[1]))
+        D = torch.tril(D,diagonal=-1) + (torch.unsqueeze(torch.abs(torch.diagonal(D,dim1=1, dim2=2)),
+                                                         dim=2) * torch.eye(D.shape[1])) + \
+            torch.transpose(torch.tril(D, diagonal=-1),dim0=1,dim1=2)
 
         #D = torch.exp(-D)
-        return D,B
+        return Compute_Block_Cholesky(D,B)

Poisson_LDS.py
@@ -61,10 +61,10 @@ class Poisson_LDS_Expected_Likelihood(nn.Module):
         if epsilon_precision is None:
             # Making this positive semi-definite
             epsilon_precision = torch.rand((zt_dim, zt_dim))
-            epsilon_precision = epsilon_precision @ epsilon_precision.T
+            epsilon_precision = epsilon_precision.T @ epsilon_precision
             # Exponentiating diagonal entries to make it positive definite
             diag_mask = torch.diag(torch.ones(epsilon_precision.shape[0]))
-            epsilon_precision = diag_mask * torch.exp(torch.diag(epsilon_precision)) + (
+            epsilon_precision = diag_mask * torch.abs(torch.diag(epsilon_precision)) + (
                         1. - diag_mask) * epsilon_precision
         self.epsilon_precision = nn.Parameter(epsilon_precision, requires_grad=True)
 

Tests/Block_Functions_Tests.py
@@ -141,10 +141,10 @@ class Test_Block_Inverse_Solve(unittest.TestCase):
         tests the block triangular solve and compares it to fully dense inverse solve implement in torch.
         """
         xt_dim = 3
-        time = 4
+        time = 10
         batch = 2
         L = torch.rand((time,xt_dim,xt_dim))
-        D = torch.exp(L@ torch.transpose(L,dim0=1,dim1=2))+5
+        D = torch.abs(L@ torch.transpose(L,dim0=1,dim1=2))
         B = torch.rand((time-1,xt_dim,xt_dim))
 
         # Need to construct cholesky decomposition to get diagonal matrix that is lower-triangular
@@ -159,15 +159,17 @@ class Test_Block_Inverse_Solve(unittest.TestCase):
         torch_inverse_solve = torch.linalg.solve(torch.repeat_interleave(torch.unsqueeze(full_mat,0)
                                                                          ,repeats = batch,dim =0), full_vec_squeeze)
         block_solve_reshaped = torch.reshape(Block_Solve,(batch,xt_dim*time))
+        print(torch.any(torch.isnan(block_solve_reshaped)))
+        print(block_solve_reshaped.shape)
         self.assertTrue(torch.allclose(torch_inverse_solve,block_solve_reshaped))
 
     def test_Block_Triangular_Solve_dimensions(self):
         """
         tests the block triangular solve and compares it to fully dense inverse solve implement in torch.
         """
-        xt_dim = 3
-        time = 4
-        batch = 2
+        xt_dim = 10
+        time = 100
+        batch = 100
         L = torch.rand((time, xt_dim, xt_dim))
         D = torch.exp(L @ torch.transpose(L, dim0=1, dim1=2)) + 5
         B = torch.rand((time - 1, xt_dim, xt_dim))
@@ -178,3 +180,30 @@ class Test_Block_Inverse_Solve(unittest.TestCase):
         # Computing block inverse
         Block_Solve = Block_Triangular_Solve(D, B, full_vec)
         self.assertTrue(tuple(Block_Solve.shape) ==(batch,time,xt_dim))
+
+    def test_Block_Triangular_Solve_big(self):
+        """
+        tests the block triangular solve and compares it to fully dense inverse solve implement in torch.
+        """
+        xt_dim = 10
+        time = 200
+        batch = 100
+        L = torch.rand((time, xt_dim, xt_dim))
+        D = torch.exp(L @ torch.transpose(L, dim0=1, dim1=2))
+        B = torch.rand((time - 1, xt_dim, xt_dim))
+
+        # Need to construct cholesky decomposition to get diagonal matrix that is lower-triangular
+        D, B = Compute_Block_Cholesky(D, B)
+        full_vec = torch.rand((batch, time, xt_dim))
+        # Computing block inverse
+        Block_Solve = Block_Triangular_Solve(D, B, full_vec)
+        # Reshaping to compute inverse with torch
+        full_mat = reshape_helper_D(D, B, lower=True)
+        full_vec_squeeze = torch.reshape(full_vec, (batch, time * xt_dim))
+        # Computing Inverse with torch
+        torch_inverse_solve = torch.linalg.solve(torch.repeat_interleave(torch.unsqueeze(full_mat, 0)
+                                                                         , repeats=batch, dim=0), full_vec_squeeze)
+        block_solve_reshaped = torch.reshape(Block_Solve, (batch, xt_dim * time))
+        print(torch.any(torch.isnan(Block_Solve)))
+        print(torch.isclose(block_solve_reshaped[0,:],torch_inverse_solve[0,:]))
+        self.assertTrue(torch.allclose(torch_inverse_solve, block_solve_reshaped))